0293d71f1f2a5474c7345338dec3556d201258d0
Shubham Jangid <shubham.jangid@acumen.aero>
2025-12-01 13:26:50 +0530

    Remove Duplicate Solution 1

---
+++ Array/logic building/remove_duplicate.py
@@ -0,0 +1,37 @@
+class Solution:
+    def removeDuplicates(self, nums) -> int:
+        # get the lenght of the array
+        n = len(nums)
+
+        i = 0
+        j = i+1
+
+        while j < n:
+            if nums[i] == nums[j]:
+                j += 1
+            else:
+                nums[i+1] = nums[j]
+                
+                i += 1
+                j += 1
+        return i+1
+        
+
+
+
+
+
+# main class
+if __name__ == "__main__":
+    nums_1 = [0, 0, 3, 3, 5, 6]
+    
+    print(f"Original Array: {nums_1}")
+    
+    # Create an instance of the Solution class
+    sol = Solution()
+    
+    # Function call to remove duplicates from array
+    k = sol.removeDuplicates(nums_1)
+    
+    print(f"Array after removing the duplicates: {nums_1[:k]}")
+    
